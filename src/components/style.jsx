ed6a780e05741bd559631d29d7ca45f7cc30d1ac
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-02-13 21:38:13 -0500

    learning MUI, understand basic cards and buttons

---
+++ src/components/style.jsx
@@ -0,0 +1,36 @@
+import { makeStyles } from "tss-react/mui";
+const useStyles = makeStyles()((theme) => {
+  return {
+    container: {
+      // backgroundColor: theme.palette.secondary.light,
+      backgroundColor: "#F7E2D9",
+      padding: theme.spacing(8, 0, 6),
+      // marginRight: theme.spacing(2)
+    },
+    icon: {
+        marginRight:'20px',
+    },
+    buttons: {
+        marginTop:'40px'
+    },
+    cardGrid:{
+        padding:'20px 0'
+    },
+    card:{
+        height:'100%',
+        display:'flex',
+        flexDirection:'column'
+    },
+    cardMedia:{
+        paddingTop:'56.25%' //16:9 ratio
+    },
+    cardContent:{
+        flexGrow:'1'
+    },
+    footer:{
+        backgroundColor:theme.palette.primary.main,
+        padding:'20px 0'
+    }
+  };
+});
+export default useStyles;
